a4f3a01b5e50a3f5a080e342a3731a8ef39c2907
Andrew <aobrien@greentube.com>
2022-06-30 15:39:48 +0100

    updates

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -20,12 +20,12 @@ import org.testng.annotations.BeforeTest;
 
 public class BrowserStackTestNGTest {
     public static WebDriver driver;
-    JavascriptExecutor jse = (JavascriptExecutor)driver;
+    JavascriptExecutor jse = (JavascriptExecutor) driver;
 
     private Local l;
 
-    @BeforeTest(alwaysRun=true)
-    @org.testng.annotations.Parameters(value={"config", "environment"})
+    @BeforeTest(alwaysRun = true)
+    @org.testng.annotations.Parameters(value = {"config", "environment"})
     public void setUp(String config_file, String environment) throws Exception {
         JSONParser parser = new JSONParser();
         JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
@@ -37,43 +37,45 @@ public class BrowserStackTestNGTest {
         Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
         Iterator it = envCapabilities.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
+            Map.Entry pair = (Map.Entry) it.next();
             capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
         }
-        
+
         Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
         it = commonCapabilities.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            if(capabilities.getCapability(pair.getKey().toString()) == null){
+            Map.Entry pair = (Map.Entry) it.next();
+            if (capabilities.getCapability(pair.getKey().toString()) == null) {
                 capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
             }
         }
 
         String username = System.getenv("BROWSERSTACK_USERNAME");
-        if(username == null) {
+        if (username == null) {
             username = (String) config.get("user");
         }
 
         String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if(accessKey == null) {
+        if (accessKey == null) {
             accessKey = (String) config.get("key");
-        }
 
-        if(capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local") == "true"){
-            l = new Local();
-            Map<String, String> options = new HashMap<String, String>();
-            options.put("key", accessKey);
-            l.start(options);
-        }
+            if (capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local") == "true") {
+                l = new Local();
+                Map<String, String> options = new HashMap<String, String>();
+                options.put("key", accessKey);
+                l.start(options);
+            }
+            String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+            capabilities.setCapability("Test build", buildName);
 
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        driver = new RemoteWebDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
+            JavascriptExecutor jse = (JavascriptExecutor) driver;
+            driver = new RemoteWebDriver(new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+        }
     }
 
-    @AfterTest(alwaysRun=true)
-    public void tearDown() throws Exception {
-        driver.quit();
-        if(l != null) l.stop();
+        @AfterTest(alwaysRun = true)
+        public void tearDown () throws Exception {
+            driver.quit();
+            if (l != null) l.stop();
+        }
     }
-}
